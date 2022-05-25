53aee350982305620cc4d70dec95b0bffd2ad162
Nik <iam@niklucky.com>
2022-05-25 19:28:38 +0300

    Added linux/amd64 platrofm  to docker build.

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -2,5 +2,5 @@
 
 VERSION=$1
 
-docker image build --tag syndicatedb/puppeteer-server:${VERSION} . 
+docker image build  --platform linux/amd64 --tag syndicatedb/puppeteer-server:${VERSION} . 
 docker tag syndicatedb/puppeteer-server:${VERSION} syndicatedb/puppeteer-server:latest
